f8d6b71a761446c9229a5046e08b18b077cb617d
Keyvan Shajari <key1.shajari@gmail.com>
2025-01-13 01:05:44 +0330

    fix page wrapper

--- src/components/wrapper/page-wrapper.tsx
+++ src/components/wrapper/page-wrapper.tsx
@@ -1,7 +1,7 @@
 import { scrollbarStyle } from "@/common/constants/constants";
 import React from "react";
 
-const sideBoxStyle = "overflow-y-auto hidden lg:block " + scrollbarStyle;
+const sideBoxStyle = "overflow-y-auto hidden lg:block col-span-2 " + scrollbarStyle;
 
 export default function PageWrapper({
   className,
@@ -11,43 +11,46 @@ export default function PageWrapper({
   rightBoxClassName,
   leftBox,
   leftBoxClassName,
-  pageTitle,
 }: {
-  className?: string | "";
+  className?: String | "";
   children?: React.ReactNode;
-  mainBoxClassName?: string | "";
+  mainBoxClassName?: String | "";
   rightBox?: React.ReactNode;
-  rightBoxClassName?: string | "";
+  rightBoxClassName?: String | "";
   leftBox?: React.ReactNode;
-  leftBoxClassName?: string | "";
-  pageTitle?: string;
+  leftBoxClassName?: String | "";
 }) {
+  let col = 6;
+  if (rightBox) col += 2;
+  if (leftBox) col += 2;
+
+  const columnsStyle = `grid-cols-${col - 2} xl:grid-cols-${col}`;
+
   return (
-    <div className={className + " w-full h-full flex flex-col items-center pt-[--appbar-height]"}>
-      {pageTitle && (
-        <div className="w-full px-4 md:px-0 bg-primary-6-light h-20 text-white flex justify-center items-center">
-          <div className="container h-20 text-2xl font-bold flex items-center">{pageTitle}</div>
-        </div>
-      )}
-      <div className="container">
-        <div className=" w-full flex flex-row h-full">
+    <div className={className + " w-full h-full"}>
+      <div className="container mx-auto h-full overflow-hidden">
+        <div className={"w-full h-full grid overflow-y-hidden gap-x-8 " + columnsStyle}>
           {rightBox && (
-            <div className={sideBoxStyle + rightBoxClassName + " ml-4"} dir="ltr">
+            <div className={sideBoxStyle + rightBoxClassName} dir="ltr">
               <div dir="rtl">{rightBox}</div>
             </div>
           )}
+
           <div
             className={
-              mainBoxClassName + " flex flex-1 h-full overflow-x-hidden  " + scrollbarStyle
+              mainBoxClassName +
+              " flex flex-1 overflow-y-auto overflow-x-hidden col-span-full lg:col-span-4 xl:col-span-6" +
+              scrollbarStyle
             }
             dir="ltr"
           >
-            <div dir="rtl" className="w-full px-4 md:px-0">
+            <div dir="rtl" className="w-full">
               {children}
             </div>
           </div>
+
           {leftBox && (
-            <div className={sideBoxStyle + leftBoxClassName + " ml-4"} dir="ltr">
+            <div className={sideBoxStyle + leftBoxClassName} dir="ltr">
               <div dir="rtl">{leftBox}</div>
             </div>
           )}
